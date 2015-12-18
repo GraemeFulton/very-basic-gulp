f62d73300167aa2b63e5e1ad7e4ac2f8529858d5
Graeme Fulton <graemefulton89@gmail.com>
2015-12-18 21:13:29 +0000

    gulp-autoprefixer

--- lesson2/gulpfile.js
+++ lesson2/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -13,6 +14,10 @@ gulp.task('styles', function(){
       .pipe(sass()
           //on sass error, don't break the build, just log an error
           .on('error', sass.logError))
+      //pipe autoprefixer
+      .pipe(autoprefixer({
+          browsers:['last 2 versions']
+      }))
       //save css to css folder
       .pipe(gulp.dest('./css'))
 
