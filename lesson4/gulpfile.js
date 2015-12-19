85032be65f7aa6e6e4f4531f8db5b825defb6a79
Graeme Fulton <graemefulton89@gmail.com>
2015-12-19 10:56:07 +0000

    browser-sync

---
+++ lesson4/gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+/**
+ * Defaul
+ * @param  {[type]} 'default' [description]
+ * @param  {[type]} function( [description]
+ * @return {[type]}           [description]
+ */
+gulp.task('default', function() {
+  // use gulp watch to listen for changes in sass, then execute styles task
+  gulp.watch('sass/**/*.scss', ['styles']);
+
+});
+
+/**
+ * browser-sync
+ * @param  {[type]} 'browser-sync' [description]
+ * @param  {[type]} function(      [description]
+ * @return {[type]}                [description]
+ */
+gulp.task('browser-sync', function(){
+
+  browserSync.init({
+    server: "./"
+  });
+
+})
+
+/**
+ * styles
+ * @param  {[type]} 'styles'  [description]
+ * @param  {[type]} function( [description]
+ * @return {[type]}           [description]
+ */
+gulp.task('styles', function(){
+
+  gulp.src('sass/**/*.scss')
+      //pipe sass to convert scss to css
+      .pipe(sass()
+          //on sass error, don't break the build, just log an error
+          .on('error', sass.logError))
+      //pipe autoprefixer
+      .pipe(autoprefixer({
+          browsers:['last 2 versions']
+      }))
+      //save css to css folder
+      .pipe(gulp.dest('./css'))
+
+})
